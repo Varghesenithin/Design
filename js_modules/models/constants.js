aeb57443ca2fd23c5e7d4a1248c304a1d5f41fe5
Varghesenithin <136353933+Varghesenithin@users.noreply.github.com>
2023-06-15 10:57:38 +0530

    Create constants.js

---
+++ js_modules/models/constants.js
@@ -0,0 +1,58 @@
+export const EXT_SEARCH_DOMAIN = 'https://x1337x.ws/sort-category-search/';
+
+export const GOOGLE_SEARCH_DOMAIN = 'https://www.google.com/search?q=';
+
+export const BING_SEARCH_DOMAIN = 'https://www.bing.com/search?q=';
+
+export const DUCKDUCKGO_SEARCH_DOMAIN = 'https://duckduckgo.com/?q=';
+
+// export const SEARCH_BUTTON_DOM =
+
+//     document.getElementsByClassName('searchButton').item(0).childNodes[1];
+
+export const BOOKMARKS_SAMPLE_URL =
+
+    'https://raw.githubusercontent.com/lscambo13/casamia/main/custom_bookmarks_sample/home-page-bookmarks-2023-1-23.json';
+
+export const WALLPAPERS_URL = './wallpapers/';
+
+export const DEF_WALLPAPER = 'space-wallpaper-20082314113712.webp';
+
+export const DEF_CUSTOM_TEXT = 'Custom Text';
+
+export const DEF_PREF =
+
+{
+
+    'bg-img-drop': 'shown',
+
+    'bg-blur-drop': 'off',
+
+    'bg-glow-drop': 'auto',
+
+    'weather-display-drop': 'off',
+
+    'footer-display-drop': 'on',
+
+    'def-widget-drop': 'casamia',
+
+    'widget-style-drop': 'minimal',
+
+    'greeting-display-drop': 'on',
+
+    'show-seconds-drop': 'off',
+
+    'clock-style-drop': '12hrs',
+
+    'am-pm-style-drop': 'uppercase',
+
+    'def-search-engine-drop': 'google',
+
+    'searchbar-position-drop': 'top',
+
+    'searchbar-color-theme-drop': 'glass',
+
+    'show-titles-drop': 'off',
+
+};
+
