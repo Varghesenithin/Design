4d2b7bc6957861dde5d842eb7a77d2416a2d1eda
Varghesenithin <136353933+Varghesenithin@users.noreply.github.com>
2023-06-15 11:21:26 +0530

    Create strings.js

---
+++ js_modules/models/strings.js
@@ -0,0 +1,51 @@
+export const cliHelpText =
+
+    `Documentation:\n
+
+    --help: This page
+
+    --fetch default : Import a predefined set of custom bookmarks.
+
+    --reset bookmarks: Deletes only the saved bookmarks.
+
+    --reset all: Reset everything, including the bookmarks and wallpaper preferences.`;
+
+export const cliUnexpectedCmdText =
+
+    `The command you have passed is invalid.\n
+
+Type --help to read the documentation.\n`;
+
+export const resetBookmarksWarningText =
+
+    `This will reset bookmarks.
+
+Make sure you have a backup to import later on.\n\n
+
+Are you sure ?`;
+
+export const resetAllWarningText =
+
+    `This will reset everything.
+
+There is no going back.\n
+
+Are you sure?`;
+
+export const askUserNameText =
+
+    `Welcome to Casa Mia
+
+What is your name?`;
+
+export const retryUserNameText =
+
+    `Please enter a valid name to proceed.
+
+Maximum length allowed is 14 characters.`;
+
+export const updateUserNameText =
+
+    `Update your name..
+
+Maximum length allowed is 14 characters.`;
