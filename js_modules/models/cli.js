8e0e1ff73a2ad1b1aa27a076296b5fd63a26f870
Varghesenithin <136353933+Varghesenithin@users.noreply.github.com>
2023-06-15 10:55:41 +0530

    Create cli.js

---
+++ js_modules/models/cli.js
@@ -0,0 +1,81 @@
+import { parseURL, resetAll, resetBookmarks } from './preferences.js';
+
+import { cliHelpText, cliUnexpectedCmdText } from './strings.js';
+
+import { fetchBookmarks } from './utils.js';
+
+export function cliCheck(input) {
+
+    // var input = getSearchTerm().value;
+
+    if (input.startsWith('--')) {
+
+        return true;
+
+    }
+
+    return false;
+
+}
+
+export function cliParse(input) {
+
+    input = input.split('--').join('');
+
+    input = input.split(' ');
+
+    // console.log(input);
+
+    switch (input[0].toLowerCase()) {
+
+        case 'help':
+
+            alert(cliHelpText);
+
+            break;
+
+        // case 'reset bookmarks':
+
+        //     resetBookmarks();
+
+        //     break;
+
+        case 'reset':
+
+            if (input[1] == 'bookmarks') resetBookmarks();
+
+            else if (input[1] == 'all') resetAll();
+
+            else alert(cliUnexpectedCmdText);
+
+            break;
+
+        case 'fetch default':
+
+            fetchBookmarks();
+
+            break;
+
+        // case 'labs':
+
+        //     toggleLabs(null);
+
+        //     break;
+
+        case 'dl':
+
+            if (input[1]) parseURL(input[1]);
+
+            // else if (input[1] == 'all') resetAll();
+
+            else alert(cliUnexpectedCmdText);
+
+            break;
+
+        default:
+
+            alert(cliUnexpectedCmdText);
+
+    }
+
+}
