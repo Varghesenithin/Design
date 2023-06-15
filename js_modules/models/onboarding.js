c77b831e40458864e0a33bc15f4fc4b217567001
Varghesenithin <136353933+Varghesenithin@users.noreply.github.com>
2023-06-15 11:05:16 +0530

    Create onboarding.js

---
+++ js_modules/models/onboarding.js
@@ -0,0 +1,64 @@
+import { DEF_PREF, DEF_WALLPAPER } from './constants.js';
+
+import { updateUserNamePreview } from './load_preferences.js';
+
+import {
+
+    askUserNameText,
+
+    retryUserNameText,
+
+    updateUserNameText,
+
+} from './strings.js';
+
+export function askUserName() {
+
+    let userName = localStorage.getItem('userName');
+
+    if (userName) {
+
+        userName = prompt(updateUserNameText, userName);
+
+        if (userName && !userName.startsWith(' ') && userName.length < 15) {
+
+            localStorage.setItem('userName', userName);
+
+            updateUserNamePreview();
+
+        };
+
+        return;
+
+    };
+
+    // for onboarding
+
+    let msg = askUserNameText;
+
+    while (!userName || userName.startsWith(' ') || userName.length > 14) {
+
+        userName = prompt(msg);
+
+        localStorage.setItem('userName', userName);
+
+        msg = retryUserNameText;
+
+    };
+
+};
+
+export function setDefaultPreferences() {
+
+    if (localStorage.getItem('onBoarding') == '1') return;
+
+    askUserName();
+
+    localStorage.setItem('advDropdownValues', JSON.stringify(DEF_PREF));
+
+    localStorage.setItem('wallpaper', DEF_WALLPAPER);
+
+    localStorage.setItem('onBoarding', '1');
+
+}
+
